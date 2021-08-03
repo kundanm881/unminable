4285926b551b0f1acf413fb10ad8de8bb8b4fd71
BuildTools <unconfigured@null.spigotmc.org>
2021-08-04 00:25:29 +0530

    unminable

---
+++ android/app/src/main/kotlin/com/kundanm881/unminable/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kundanm881.unminable
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/my_strings.dart
@@ -0,0 +1,14 @@
+class MyStrings {
+ final baseUrl = 'https://api.unminable.com/v4';
+
+  // GET /pool
+  // GET /coin
+  // GET /coin/:coin
+  // GET /address/:address?coin=:coin
+  // GET /account/:uuid
+  // GET /account/:uuid/stats
+  // GET /account/:uuid/workers
+  // GET /account/:uuid/payments
+  // GET /account/:uuid/referrals
+ // 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 github Token
+}
